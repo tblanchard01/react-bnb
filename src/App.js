8ee751671c0653c7a56b49660c839d7debb3fe8b
Tim Blanchard <tim.blanchard@hotmail.co.uk>
2018-08-19 21:55:46 +0100

    bg image and description working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Flat from './components/flat';
 
 class App extends Component {
   render() {
+    const flat = {
+      "id": 148,
+      "name": "Trendy Apt in Buttes Montmartre",
+      "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat2.jpg",
+      "price": 200,
+      "priceCurrency": "EUR",
+      "lat": 48.885707,
+      "lng": 2.343543
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Flat flat={flat} />
       </div>
     );
   }
--- src/components/hello.js
+++ src/components/hello.js
@@ -1,2 +1,9 @@
 import React from "react";
- 
+  
+class Hello extends React.Component{
+    render() {
+        return <div>Hello {this.props.name}  </div>
+    }
+}
+
+export default Hello  
